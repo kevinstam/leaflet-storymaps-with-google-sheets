9c7c00ab21323e77115b4a6ef7ec6a7b08b0a950
kevinstam <50917481+kevinstam@users.noreply.github.com>
2019-05-22 16:35:12 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1bRezUXBMw-o9GnfMfIIUNbj5UEdU6W4eQecCvuUdVyw/edit#gid=0';
